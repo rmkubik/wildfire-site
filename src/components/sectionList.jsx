d349530234b7c4d1344747e7ba8e77b8222a476c
Ryan Kubik <rmkubik@me.com>
2021-04-05 18:52:42 -0700

    Update images on presskit and site

--- src/components/sectionList.jsx
+++ src/components/sectionList.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import sectionListStyles from './sectionList.module.scss';
-import Image from './images';
+import React from "react";
+import sectionListStyles from "./sectionList.module.scss";
+import Image from "./images";
 
 const SectionList = ({ links }) => {
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ display: 'block', flexShrink: '0', marginRight: '3rem' }}>
+    <div style={{ display: "flex" }}>
+      <div style={{ display: "block", flexShrink: "0", marginRight: "3rem" }}>
         <div className={sectionListStyles.header}>
           {links.map(({ label, link }) => (
             <a href={link}>{label}</a>
           ))}
         </div>
       </div>
-      <div className={sectionListStyles.imageContainer}>
-        <Image src="titleScreenGif" alt="Wildfire title screen" />
+      <div
+        className={`${sectionListStyles.imageContainer} ${sectionListStyles.squareAspectRatio}`}
+      >
+        <div className={sectionListStyles.inner}>
+          <Image src="libraryCapsule" alt="library capsule image" />
+        </div>
       </div>
     </div>
   );
